8d49a23ee70a6fd697b5f8b7f963bd8fe7998c9a
Stuart Tottle <stuarttottle@hotmail.com>
2021-08-16 20:47:15 +0100

    revert change

--- apps/jitsi-meet-react/src/main.tsx
+++ apps/jitsi-meet-react/src/main.tsx
@@ -15,7 +15,7 @@ import {
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app/App1';
+import App from './app/App';
 
 library.add(
   faCheckSquare,
