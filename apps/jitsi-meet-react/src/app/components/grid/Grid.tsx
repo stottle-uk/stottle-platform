ce62c253621987bf06becf71e88e0504229233e7
Stuart Tottle <stuarttottle@hotmail.com>
2021-08-23 08:23:10 +0100

    updated grid

--- apps/jitsi-meet-react/src/app/components/grid/Grid.tsx
+++ apps/jitsi-meet-react/src/app/components/grid/Grid.tsx
@@ -4,53 +4,21 @@ import GridControls from './GridControls';
 import GridFooter from './GridFooter';
 import GridItem from './GridItem';
 
-const getCols = (len: number): string => {
-  if (len <= 2) {
-    return `repeat(${len}, 1fr)`;
-  }
-  if (len <= 4) {
-    return `repeat(2, 1fr)`;
-  }
-  if (len <= 9) {
-    return `repeat(3, 1fr)`;
-  }
-  return `repeat(4, 1fr)`;
-};
-
-const getRows = (len: number): string => {
-  if (len <= 2) {
-    return '1fr';
-  }
-  if (len <= 4) {
-    return '1fr 1fr';
-  }
-  if (len <= 6) {
-    return '1fr 1fr';
-  }
-  if (len <= 12) {
-    return '1fr 1fr 1fr';
-  }
-  return `repeat(4, 1fr)`;
-};
-
 const Grid: React.FC = () => {
   const { allTracks, participantsLength } = useJitsiTracks('ME!');
 
   return (
-    <div
-      className="grid-container"
-      style={{
-        gridTemplateColumns: getCols(participantsLength),
-        gridTemplateRows: getRows(participantsLength)
-      }}
-    >
+    <div className={`grid-container grid-${participantsLength}`}>
       {allTracks.map(
-        user =>
+        (user, i) =>
           user.tracks.audio &&
           user.tracks.video && (
             <GridItem
               key={user.userId}
               className="grid-item"
+              style={{
+                gridArea: `box${i + 1}`
+              }}
               username={user.username}
               audio={user.tracks.audio}
               video={user.tracks.video}
