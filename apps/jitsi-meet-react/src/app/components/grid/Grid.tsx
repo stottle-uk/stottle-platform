a136870edffc579d143f6963050948a40d386910
Stuart Tottle <stuarttottle@hotmail.com>
2021-08-31 20:51:43 +0100

    set focused user

--- apps/jitsi-meet-react/src/app/components/grid/Grid.tsx
+++ apps/jitsi-meet-react/src/app/components/grid/Grid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useJitsiTracks } from '../../hooks/useJitsiTracks';
 import GridControls from './GridControls';
 import GridFooter from './GridFooter';
@@ -6,22 +6,39 @@ import GridItem from './GridItem';
 
 const Grid: React.FC = () => {
   const { allTracks, participantsLength } = useJitsiTracks('ME!');
+  const [focusedUser, setFocusedUser] = useState<string>();
+
+  const getItemStyle = (userId: string, i: number) =>
+    focusedUser === userId ? 'boxFocused' : `box${i + 1}`;
+
+  const getItemClass = (focusedUser: string | undefined, userId: string) =>
+    `grid-item ${focusedUser && focusedUser !== userId ? 'hidden' : ''} ${
+      focusedUser === userId ? 'vid-contain' : ''
+    }`;
+
+  const updateFocusedUser = (userId: string) =>
+    setFocusedUser(id => (id === userId ? undefined : userId));
+
+  const gridClass = `grid-container ${
+    focusedUser ? 'grid-focused' : `grid-${participantsLength}`
+  }`;
 
   return (
-    <div className={`grid-container grid-${participantsLength}`}>
+    <div className={gridClass}>
       {allTracks.map(
         (user, i) =>
           user.tracks.audio &&
           user.tracks.video && (
             <GridItem
               key={user.userId}
-              className="grid-item"
+              className={getItemClass(focusedUser, user.userId)}
               style={{
-                gridArea: `box${i + 1}`
+                gridArea: getItemStyle(user.userId, i)
               }}
               username={user.username}
               audio={user.tracks.audio}
               video={user.tracks.video}
+              onDoubleClick={() => updateFocusedUser(user.userId)}
             >
               <GridFooter
                 className="grid-footer"
