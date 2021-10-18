534015fffd9b9aaaf60ba5d832d24556d188e403
Stuart Tottle <stuarttottle@hotmail.com>
2021-10-18 09:47:14 +0100

    added year picker

--- apps/calander-test/src/app/app.tsx
+++ apps/calander-test/src/app/app.tsx
@@ -1,31 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './app.module.scss';
 import { buildCalander, DATSTUFF, mapDate } from './calander-helpers';
 
-const calander = buildCalander(2012, 11, mapDate);
+// const calander = buildCalander(2021, 11, mapDate);
 
 export function App() {
   // const days = 0;
-  const [selected, setSelected] = useState(new Date().getMonth() + 1);
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
+  const [calander, setClander] = useState(buildCalander(2021, 11, mapDate));
   // const [selected, setSelected] = useState('1');
+
+  useEffect(() => {
+    setClander(buildCalander(selectedYear, 11, mapDate));
+  }, [selectedYear]);
 
   const getClass = (currMonth: number, val: DATSTUFF) =>
     `${styles.app} ${currMonth === val.month ? '' : styles.fade}`;
 
   return (
     <div>
-      <select
-        value={selected}
-        onChange={e => setSelected(+e.currentTarget.value)}
-      >
-        {Object.keys(calander).map(k => (
-          <option key={k} value={k}>
-            {k}
-          </option>
-        ))}
-      </select>
+      <div>
+        <select
+          value={selectedYear}
+          onChange={e => setSelectedYear(+e.currentTarget.value)}
+        >
+          {[...new Array(25).keys()].map(k => (
+            <option key={k} value={2000 + k}>
+              {2000 + k}
+            </option>
+          ))}
+        </select>
+        <select
+          value={selectedMonth}
+          onChange={e => setSelectedMonth(+e.currentTarget.value)}
+        >
+          {Object.keys(calander).map(k => (
+            <option key={k} value={k}>
+              {k}
+            </option>
+          ))}
+        </select>
+      </div>
       {Object.entries(calander)
-        .filter(([key]) => +key === selected)
+        .filter(([key]) => +key === selectedMonth)
         .map(([key, d]) => (
           <div className={styles.app} key={key}>
             {d.map((f, j) => (
