08974dbc25cede169af8f78617d5c1c15e024407
Stuart Tottle <stuarttottle@hotmail.com>
2021-12-28 13:52:13 +0000

    added analytics

--- apps/stottle-web/pages/_app.tsx
+++ apps/stottle-web/pages/_app.tsx
@@ -1,9 +1,25 @@
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import './index.scss';
 import './styles.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const app = initializeApp({
+      apiKey: 'AIzaSyDkMRYsylELwrZLWu4XlBMvvQeVpZMViV0',
+      authDomain: 'stottle-web.firebaseapp.com',
+      projectId: 'stottle-web',
+      storageBucket: 'stottle-web.appspot.com',
+      messagingSenderId: '814577459797',
+      appId: '1:814577459797:web:28dc22587c940e61c8eadc',
+      measurementId: 'G-CSHBBRQ96C'
+    });
+    getAnalytics(app);
+  }, []);
+
   return (
     <>
       <Head>
