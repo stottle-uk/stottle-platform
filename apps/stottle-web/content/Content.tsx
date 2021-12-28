ecabb497f3380e182feafec6ffc60df293f46a20
Stuart Tottle <stuarttottle@hotmail.com>
2021-12-28 14:07:28 +0000

    fixed images

--- apps/stottle-web/content/Content.tsx
+++ apps/stottle-web/content/Content.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import styles from './Content.module.scss';
 import ContentItem from './ContentItem';
-import github from './logos/logo-github.svg';
-import linkedin from './logos/logo-linked-in.svg';
-import medium from './logos/logo-medium.svg';
 
 const items = [
   {
     title: 'Writing',
-    logo: medium,
-    link: 'https://medium.com/@stuarttottle',
+    logo: '/logos/logo-medium.svg',
+    link: 'https://medium.com/@stuarttottle'
   },
   {
     title: 'Code',
-    logo: github,
-    link: 'https://github.com/stottle-uk',
+    logo: '/logos/logo-github.svg',
+    link: 'https://github.com/stottle-uk'
   },
   {
     title: 'Profile',
-    logo: linkedin,
-    link: 'https://www.linkedin.com/in/stuarttottle/',
-  },
-].map((i) => <ContentItem key={i.title} item={i} />);
+    logo: '/logos/logo-linked-in.svg',
+    link: 'https://www.linkedin.com/in/stuarttottle/'
+  }
+].map(i => <ContentItem key={i.title} item={i} />);
 
 const Content: React.FC = () => <div className={styles.content}>{items}</div>;
 
