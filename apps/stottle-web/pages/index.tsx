51a0efd5c5be8ea23acdf1fa7b3b69f868e5e8cf
Stuart Tottle <stuarttottle@hotmail.com>
2021-12-28 13:15:17 +0000

    try an update

--- apps/stottle-web/pages/index.tsx
+++ apps/stottle-web/pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.scss';
 
 export function Index() {
-  return <div className={styles.page}>Stuart Tottle</div>;
+  return <div className={styles.page}>Stuart Tottle With Edit</div>;
 }
 
 export default Index;
